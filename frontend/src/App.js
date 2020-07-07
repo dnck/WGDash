3c58396b7c089a820d160f5aefdea4c80532b2b3
cjhines <chrishinesabdn@gmail.com>
2020-07-07 16:39:29 +0200

    Added fetch test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import newsFetch from './services/newsFetch'
 import './App.css';
 
 function App() {
+
+  newsFetch()
+    .then(fuck => console.log(fuck))
+
   return (
     <div className="App">
       <header className="App-header">
