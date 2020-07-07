f49422fdea0036fe66fda6bb8b5fddd1c562d816
Dan John Cook <cookdj0128@gmail.com>
2020-07-07 13:26:51 +0200

    Add get_source method

--- server.py
+++ server.py
@@ -59,5 +59,40 @@ def get_weather():
     wm = weather_man.WeatherMan(key)
     return wm.get_weather(), 200
 
+"""
+POST add new source
+ -> present full rss json
+ -> select fields
+ -> POST fields for new source
+update new source <-
+"""
+
+@APP.route("/add-source/", methods=["POST"])
+def add_source():
+    source = request.get_json()
+    url = source.get("url")
+    if not url:
+        return {"Missing Data": "need 'url' field in data"}, 400
+    source_reader = rss_man.RssReader()
+    feed = source_reader.get_feed(url)
+    result_set = []
+    for entry in feed["entries"]:
+        result_set.append(entry)
+    return result_set, 200
+
+@APP.route("/parse-source/", methods=["POST"])
+def parse_new_source():
+    source = request.get_json()
+    summary_field_key = source.get("summary_key")
+    title_field_key = source.get("title_key")
+    url_field_key = source.get("url_key")
+    more_keys = source.get("keys")
+
+
+
+
+
+
+
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", debug=True)
---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -i -H "Content-Type: application/json" -X POST -d '{"url":"https://www.reddit.com/r/gonewildstories.rss"}' http://127.0.0.1:5000/add-source
