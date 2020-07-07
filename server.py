ebbe303dc5adab13facfaf3ea9d460642d20a344
Dan John Cook <cookdj0128@gmail.com>
2020-07-07 11:43:04 +0200

    Basic hello world working

--- server.py
+++ server.py
@@ -13,4 +13,27 @@ import sys
 
 from flask import Flask, request
 
-print("Hello World!")
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+
+# >> The application is a small service.
+APP = Flask(__name__)
+
+@APP.route("/news/", methods=["GET"])
+def get_news():
+    """route_endpoint
+
+    >> The service accepts data from a GPS tracker device.
+    >> In the beginning of a track, the service requests a route to be created...
+    """
+    APP.logger.debug("News feeds requested.")
+    news = {"Hello": "World!"}
+    return json.dumps(news), 200
+
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", debug=True)
